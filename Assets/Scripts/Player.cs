0f714c7c81f5ec6a91c93116ff4e4b50648206f5
Lucas Lima <lucas85235@gmail.com>
2021-07-28 08:45:48 -0400

    Teste

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,31 +6,32 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed;
-    Vector2 moveInput;
-    
-    Animator anim;
     public Rigidbody2D rb;
-    SpriteRenderer sprite;
 
-    // Start is called before the first frame update
+    Vector2 moveInput;
+    SpriteRenderer sprite;
+    Animator anim;
+
     void Start()
     {
         anim = GetComponentInChildren<Animator>();
         sprite = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        Move();
+    }
+
+    private void Move()
     {
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
 
         transform.position = transform.position + movement * Speed * Time.deltaTime;
-
         sprite.flipX = (mousePos.x < screenPoint.x);
-
-        anim.SetBool("isMoving", (Mathf.Abs(moveInput.x) > 0 || Mathf.Abs(moveInput.y) > 0));
+        
+        anim?.SetBool("isMoving", (Mathf.Abs(moveInput.x) > 0 || Mathf.Abs(moveInput.y) > 0));        
     }
 }
--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
+public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float destroyTime = 2f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, destroyTime);
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.right * Time.deltaTime * speed);
